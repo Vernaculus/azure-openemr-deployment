8a59a4c096d39148a5bfc4d8a8d28c6031487dcb
Josh <josh@dad-dell-5500.Home>
2025-12-18 13:46:54 -0500

    feat: Added code to main.tf for Azure Resource Group deployment.

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Disable automatic provider registration since the SP lacks subscription-level rights
+  resource_provider_registrations = "none"
+
+}
+
+resource "azurerm_resource_group" "openemr_dev" {
+  name     = "rg-openemr-dev-eus"
+  location = "eastus"
+
+  tags = {
+    env        = "dev"
+    app        = "openemr"
+    owner      = "Josh Hall"
+    costCenter = "personal-lab"
+    compliance = "hipaa-pcidss"
+  }
+}
+ 
